c158f3e245c862ed5c28987bdf0f0ec1d68febf7
Taelynn Knutson <taelynnknutson@Taelynns-MacBook-Pro.local>
2018-04-18 17:35:29 -0700

    Ernesto can fly!!!!

---
+++ animationsbees.js
@@ -0,0 +1,12 @@
+var ernestoTheBee = document.getElementById("ernesto");
+var container = document.getElementById("container");
+var movemouse = document.getElementById("movemouse");
+container.addEventListener("mousemove", moveErnesto);
+
+function moveErnesto(event) {
+    var xPosition = event.clientX - container.getBoundingClientRect().left - (ernestoTheBee.clientWidth / 2);
+	var yPosition = event.clientY - container.getBoundingClientRect().top - (ernestoTheBee.clientHeight / 2);
+	// in case of a wide border, the boarder-width needs to be considered in the formula above
+	ernestoTheBee.style.left = xPosition + "px";
+	ernestoTheBee.style.top = yPosition + "px";
+}
