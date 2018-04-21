84c1c5324d1074c3f5008a1c177c984f30f80a01
Taelynn Knutson <taelynnknutson@Taelynns-MacBook-Pro.local>
2018-04-20 21:25:40 -0700

    trying to get points function to work

--- taelynn.js
+++ taelynn.js
@@ -1,8 +1,8 @@
 var flowerButtons = document.getElementsByClassName("flowerButtons");
 
-function addPoints {
-
+function addPoints() {
+    var points = document.getElementById('points');
+    var newPoints = points.value + 10;
+    console.log(newPoints);
+    flowerButtons.addEventListener("click", addPoints);
 }
-
-
-flowerButtons.addEventListener("click", addPoints);
