afb03871c4d1525d845e6778bebf8f7b4d3665a7
Taelynn Knutson <taelynnknutson@Taelynns-MacBook-Pro.local>
2018-04-20 22:52:37 -0700

    score thing works!

--- taelynn.js
+++ taelynn.js
@@ -1,8 +1,22 @@
-var flowerButtons = document.getElementsByClassName("flowerButtons");
-
-function addPoints() {
-    var points = document.getElementById('points');
-    var newPoints = points.value + 10;
-    console.log(newPoints);
-    flowerButtons.addEventListener("click", addPoints);
+var flowerButtons = document.getElementsByClassName("flower");
+addPoints = function(event) {
+    var startingPoints = Number(document.getElementById('points').innerHTML);
+    startingPoints = startingPoints + 10;
+    console.log(startingPoints);
+    document.getElementById('points').innerHTML = startingPoints;
 }
+flowerButtons[0].addEventListener('click', addPoints);
+
+
+for (var i = 0; i < flowerButtons.length; i++) {
+    flowerButtons[i].addEventListener('click', addPoints, true);
+    console.log(i);
+}
+///addPoints();
+
+
+
+/// clicking on ernesto
+document.getElementById('ernesto').addEventListener('click', function (){
+    console.log("click");
+});
