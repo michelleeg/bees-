9c7ed0bfa1839e1e351e52d260e1dc7f4daf5d3a
michelleeg <michelleisflawsome@gmail.com>
2018-04-23 12:12:58 -0700

    got ernesto's position

--- michellebee.js
+++ michellebee.js
@@ -1,6 +1,3 @@
-function collision {
-    var backgroundColor = getElementById("ernesto");
-
 var container = {x: 100, y: 0, width: 900, height: 600};
 var ernesto = {x: 428, y: 530, width: 45, height: 45};
 var cloud1 = {x: 2.5, y: 0, width: 120, height: 90};
@@ -11,4 +8,19 @@ var cloud4 = {x: 302.5, y: 775, width: 120, height: 90};
 if (ernesto.x < cloud1.x + cloud1.width && ernesto.x + ernesto.width > cloud1.x && ernesto.y < cloud1.y + cloud1.height && ernesto.height + ernesto.y > cloud1.y) {
 
 }
+var ernestoTheBee = document.getElementById("ernesto");
+var container = document.getElementById("container");
+var movemouse = document.getElementById("movemouse");
+//container.addEventListener("mousemove", moveErnesto);
+var beeposition = document.getElementById("beeposition");
+container.addEventListener("mousemove", ernestoPosition);
+
+function ernestoPosition(event) {
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+    beeposition.textContent = mouseX + ", " + mouseY;
+}
+
+function detectCollision(event) {
+    va
 }
