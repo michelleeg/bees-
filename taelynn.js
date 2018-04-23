cd56a692b8c135df3381a7b17dc1f1cf532c1b7e
Taelynn Knutson <taelynnknutson@Taelynns-MacBook-Pro.local>
2018-04-22 17:45:31 -0700

    get 50 bonus points for making to beehive

--- taelynn.js
+++ taelynn.js
@@ -1,22 +1,18 @@
 var flowerButtons = document.getElementsByClassName("flower");
-addPoints = function(event) {
-    var startingPoints = Number(document.getElementById('points').innerHTML);
-    startingPoints = startingPoints + 10;
-    console.log(startingPoints);
-    document.getElementById('points').innerHTML = startingPoints;
-}
-flowerButtons[0].addEventListener('click', addPoints);
-
+var beeHive = document.getElementById("beehive");
 
 for (var i = 0; i < flowerButtons.length; i++) {
     flowerButtons[i].addEventListener('click', addPoints, true);
-    console.log(i);
 }
-///addPoints();
 
-
-
-/// clicking on ernesto
-document.getElementById('ernesto').addEventListener('click', function (){
-    console.log("click");
-});
+function addPoints(event) {
+    var startingPoints = Number(document.getElementById('points').innerHTML);
+    startingPoints = startingPoints + 10;
+    document.getElementById('points').innerHTML = startingPoints;
+}
+function addBeeHivePoints (event) {
+    var startingPoints = Number(document.getElementById('points').innerHTML);
+    startingPoints = startingPoints + 50;
+    document.getElementById('points').innerHTML = startingPoints;
+}
+beeHive.addEventListener("click", addBeeHivePoints);
