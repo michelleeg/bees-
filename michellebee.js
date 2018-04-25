bca119fb852a0374cb2744414da7a3337d4ebbcc
michelleeg <michelleisflawsome@gmail.com>
2018-04-25 10:48:50 -0700

    started on cloud position

--- michellebee.js
+++ michellebee.js
@@ -1,4 +1,4 @@
-var container = {x: 100, y: 0, width: 900, height: 600};
+/*var container = {x: 100, y: 0, width: 900, height: 600};
 var ernesto = {x: 428, y: 530, width: 45, height: 45};
 var cloud1 = {x: 2.5, y: 0, width: 120, height: 90};
 var cloud2 = {x: 102.5, y: 775, width: 120, height: 90};
@@ -7,20 +7,23 @@ var cloud4 = {x: 302.5, y: 775, width: 120, height: 90};
 
 if (ernesto.x < cloud1.x + cloud1.width && ernesto.x + ernesto.width > cloud1.x && ernesto.y < cloud1.y + cloud1.height && ernesto.height + ernesto.y > cloud1.y) {
 
-}
+}*/
 var ernestoTheBee = document.getElementById("ernesto");
 var container = document.getElementById("container");
 var movemouse = document.getElementById("movemouse");
 //container.addEventListener("mousemove", moveErnesto);
 var beeposition = document.getElementById("beeposition");
 container.addEventListener("mousemove", ernestoPosition);
+var cloud1position = document.getElementById("cloud1");
 
 function ernestoPosition(event) {
     var mouseX = event.clientX;
     var mouseY = event.clientY;
     beeposition.textContent = mouseX + ", " + mouseY;
+    var cloud1x = cloud1.style.top;
+    cloud1.textcontent = cloud1x;
 }
 
 function getPosition(cloud1) {
-    var p
+
 }
