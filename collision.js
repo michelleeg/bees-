e275c80da8b1681099709d32f0f99990cd029e29
michelleeg <michelleisflawsome@gmail.com>
2018-04-29 21:16:56 -0700

    deleted the commented out part

--- collision.js
+++ collision.js
@@ -1,13 +1,3 @@
-/*var container = {x: 100, y: 0, width: 900, height: 600};
-var ernesto = {x: 428, y: 530, width: 45, height: 45};
-var cloud1 = {x: 2.5, y: 0, width: 120, height: 90};
-var cloud2 = {x: 102.5, y: 775, width: 120, height: 90};
-var cloud3 = {x: 202.5, y: 0, width: 120, height: 90};
-var cloud4 = {x: 302.5, y: 775, width: 120, height: 90};
-
-if (ernesto.x < cloud1.x + cloud1.width && ernesto.x + ernesto.width > cloud1.x && ernesto.y < cloud1.y + cloud1.height && ernesto.height + ernesto.y > cloud1.y) {
-
-}*/
 var ernestoTheBee = document.getElementById("ernesto");
 var container = document.getElementById("container");
 var movemouse = document.getElementById("movemouse");
@@ -33,7 +23,6 @@ function ernestoPosition(event) {
    if ((mouseX > cloud2_current_pos && (mouseX < cloud2_current_pos + 120)) && (mouseY > 200 && mouseY < 290)) {
         playAgainModal();
     }
-    //cloud1position.textContent = "[X]";
     if ((mouseX > cloud3_current_pos && (mouseX < cloud3_current_pos + 120)) && (mouseY > 300 && mouseY < 390)) {
         playAgainModal();
     }
